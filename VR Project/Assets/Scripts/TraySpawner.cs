9cd8f2dff0b1defe52d68480465608112791850d
Guillem Alqueza Mayench <guillemalqueza@gmail.com>
2025-05-18 20:52:16 +0200

    Tray spawner

---
+++ VR Project/Assets/Scripts/TraySpawner.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+public class TraySpawner : MonoBehaviour
+{
+    public event EventHandler OnTraySpawned;
+    public event EventHandler OnTrayRemoved;
+
+    private float spawnTrayTimer;
+    [SerializeField] private float spawnTrayTimerMax = 5f;
+    private int trayCount;
+    [SerializeField] private int trayCountMax = 3;
+
+    private void Update()
+    {
+        if (trayCount >= trayCountMax)
+            return;
+
+        spawnTrayTimer += Time.deltaTime;
+        if (spawnTrayTimer >= spawnTrayTimerMax)
+        {
+            spawnTrayTimer = 0f;
+            SpawnTray();
+        }
+    }
+
+    public void SpawnTray()
+    {
+        if (trayCount < trayCountMax)
+        {
+            trayCount++;
+            OnTraySpawned?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ VR Project/Assets/Scripts/TraySpawnerVisual.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraySpawnerVisual : MonoBehaviour
+{
+    [SerializeField] private TraySpawner traySpawner;
+    [SerializeField] private Transform topPoint;
+    [SerializeField] private Transform trayVisualPrefab;
+    [SerializeField] private float trayOffsetY = 0.05f;
+
+    private List<GameObject> trayVisualGameObjectList;
+
+    private void Awake()
+    {
+        trayVisualGameObjectList = new List<GameObject>();
+    }
+
+    private void Start()
+    {
+        traySpawner.OnTraySpawned += TraySpawner_OnTraySpawned;
+        traySpawner.OnTrayRemoved += TraySpawner_OnTrayRemoved;
+
+        traySpawner.SpawnTray();
+    }
+
+    private void TraySpawner_OnTrayRemoved(object sender, System.EventArgs e)
+    {
+        GameObject trayGameObject = trayVisualGameObjectList[trayVisualGameObjectList.Count - 1];
+        trayVisualGameObjectList.Remove(trayGameObject);
+        Destroy(trayGameObject);
+    }
+
+    private void TraySpawner_OnTraySpawned(object sender, System.EventArgs e)
+    {
+        Transform trayVisualTransform = Instantiate(trayVisualPrefab, topPoint);
+
+        trayVisualTransform.localPosition = new Vector3(0, trayOffsetY * trayVisualGameObjectList.Count, 0);
+
+        trayVisualGameObjectList.Add(trayVisualTransform.gameObject);
+    }
+}
