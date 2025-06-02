24cc139efc50b7cd31de0b66cb2c527b07c7c35c
Sergio <sergiof.sf88@gmail.com>
2025-06-02 22:59:51 +0200

    bug

--- VR Project/Assets/Scripts/CheckOrders.cs
+++ VR Project/Assets/Scripts/CheckOrders.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class CheckOrders : MonoBehaviour
 {
-    public void CheckTrayWithOrder
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
